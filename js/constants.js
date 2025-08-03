2ac3445dd60cca3d3667be417e45d466a7a41380
Pinchoboo <pinchoboo@gmail.com>
2025-08-03 18:01:54 +0200

    raise default difficulty

--- js/constants.js
+++ js/constants.js
@@ -2,5 +2,5 @@ const DEFAULT_ANYONE_CAN_START = true;
 const DEFAULT_SECONDS = 15;
 const DEFAULT_LIVES = 3;
 const DEFAULT_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-const DEFAULT_MINRARITY = 0
+const DEFAULT_MINRARITY = 10
 const DEFAULT_MAXRARITY = 60
